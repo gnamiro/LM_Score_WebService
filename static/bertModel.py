1e61b8c11ab00b56f8648bf1ebedf81f49782cb6
gnamiro <naeimghahramanpour@gmail.com>
2022-03-29 01:30:31 +0430

    Optimize and refactor model

--- static/bertModel.py
+++ static/bertModel.py
@@ -32,83 +32,103 @@ class PersianMaskedModel:
         self.bertMaskedLM = BertForMaskedLM.from_pretrained(
             'Model/bert-base-parsbert-uncased', return_dict=True)
 
-    def get_word_in_sentence_probability(
-        self,
-        sentence: str,
-        word: str,
-        bert_model: BertForMaskedLM,
-        bert_tokenizer: BertTokenizer,
-        word_index: int = -1,
-    ) -> Tuple[Tuple[str, float], ...]:
-        whole_tokens = word_tokenize(sentence)
-        # whole_tokens = tokenizer.tokenize(sentence)
-        # print(whole_tokens)
-        if word_index == -1:
-            word_index = whole_tokens.index(word)
-        bert_token_map = {
-            idx: bert_tokenizer.encode(whole_token, add_special_tokens=False)
-            for idx, whole_token in enumerate(whole_tokens)
-        }  # type: Dict[int,List[int]]
-        # print(bert_token_map)
-        mask_token_id = bert_tokenizer.encode(
-            "[MASK]", add_special_tokens=False)
-        tokens_to_predict = bert_tokenizer.encode(
-            word, add_special_tokens=False)
-        bert_token_map[word_index] = mask_token_id * \
-            len(tokens_to_predict)  # type: ignore
-        # print(bert_token_map)
-        # LOG.debug(
-        #     "total bert tokens: %s whole tokens: %s",
-        #     len(list(chain.from_iterable(bert_token_map.values()))),
-        #     len(whole_tokens),
-        # )
-        torch.set_grad_enabled(False)
-        bert_model.eval()
-        # to find the true index of the desired word; count all of the tokens and subtokens before
-        starting_position = (
+    def find_first_index_of_word(self, token_map: dict, word_index: int) -> int:
+        return (
             len(
                 list(
                     chain.from_iterable(
                         [
-                            vals for key, vals in bert_token_map.items() if key < word_index
-                        ]  # type : ignore
+                            vals for key, vals in token_map.items() if key < word_index
+                        ]
                     )
                 )
             )
             + 1
         )
-        start_token_id = bert_tokenizer.encode(
-            "[CLS]", add_special_tokens=False)
-        end_token_id = bert_tokenizer.encode("[SEP]", add_special_tokens=False)
-        the_tokens = list(chain.from_iterable(bert_token_map.values()))
-        # LOG.debug(bert_tokenizer.convert_ids_to_tokens(the_tokens))
-        indexed_tokens = list(start_token_id) + the_tokens + list(end_token_id)
+
+    def get_masked_sentence_score(self, indexed_tokens: list) -> torch.tensor:
+        torch.set_grad_enabled(False)
+        self.bertMaskedLM.eval()
+
         softmax = torch.nn.Softmax(dim=1)
         with torch.no_grad():
-            # pylint: disable=not-callable,no-member
             tokens_tensor = torch.tensor([indexed_tokens])
             segments_tensors = torch.tensor(
-                # type: ignore
                 [torch.zeros(len(indexed_tokens), dtype=int).tolist()]
             )
-            outputs = bert_model(
+            outputs = self.bertMaskedLM(
                 tokens_tensor, token_type_ids=segments_tensors)
             predictions = softmax(outputs[0].squeeze(0))
+
+        return predictions
+
+    def tokenize_sentence(self, token_map: dict) -> list:
+        start_token_id = self.tokenizer.encode(
+            "[CLS]", add_special_tokens=False)
+        end_token_id = self.tokenizer.encode("[SEP]", add_special_tokens=False)
+        the_tokens = list(chain.from_iterable(token_map.values()))
+
+        return list(start_token_id) + the_tokens + list(end_token_id)
+
+    def get_word_prediction_score(
+        self,
+        tokens_to_predict,
+        predictions,
+        starting_position,
+    ) -> Tuple[Tuple[str, float], ...]:
+
         if len(tokens_to_predict) == 1:
-            # type: ignore
             return tuple([predictions[starting_position][tokens_to_predict].item()])
+
         return tuple(
             [
                 (
-                    bert_tokenizer.convert_ids_to_tokens(tmp),
-                    predictions[starting_position +
-                                idx][tmp].item(),  # type: ignore
+                    self.tokenizer.convert_ids_to_tokens(tmp),
+                    predictions[starting_position][tmp].item(),  # type: ignore
                 )
                 for idx, tmp in enumerate(tokens_to_predict)
             ]
         )  # type: ignore
 
-    def sum_log_probabilities_word(self, results: Tuple, boost_factor: int = 100) -> float:
+    def get_words_in_sentence_probability(
+        self,
+        sentence: str,
+        words: list,
+        word_index: int,
+    ) -> Tuple[Tuple[str, float], ...]:
+
+        whole_tokens = word_tokenize(sentence)
+
+        bert_token_map = {
+            idx: self.tokenizer.encode(whole_token, add_special_tokens=False)
+            for idx, whole_token in enumerate(whole_tokens)
+        }  # type: Dict[int,List[int]]
+
+        mask_token_id = self.tokenizer.encode(
+            "[MASK]", add_special_tokens=False)
+
+        words_tokens_to_predict = {}
+        for word in words:
+            words_tokens_to_predict[word] = self.tokenizer.encode(
+                word, add_special_tokens=False)
+
+        bert_token_map[word_index] = mask_token_id * 1
+
+        indexed_tokens = self.tokenize_sentence(bert_token_map)
+
+        predictions = self.get_masked_sentence_score(indexed_tokens)
+
+        # to find the true index of the desired word; count all of the tokens and subtokens before
+        starting_position = self.find_first_index_of_word(
+            bert_token_map, word_index)
+        word_probs = []
+        for word, tokens_to_predict in words_tokens_to_predict.items():
+            word_probs.append((word, self.get_word_prediction_score(
+                tokens_to_predict, predictions, starting_position)))
+
+        return word_probs
+
+    def sum_log_probabilities_word(self, results: Tuple, boost_factor: int = 1) -> float:
         if(len(results) > 1):
             return sum([math.log(tmp[1] * boost_factor) for tmp in results])
 
@@ -121,15 +141,15 @@ class PersianMaskedModel:
         # suggestion_words = convert_suggestions_to_list(targets)
         # print(suggestion_words)
 
+        sentence = sentenceMask.replace(self.tokenizer.mask_token, 'یکانان')
+        words_probabilities = self.get_words_in_sentence_probability(
+            sentence, targets,  word_index=mask_index)
         results_log = []
-
-        for target_word in targets:
-            sentence = sentenceMask.replace(
-                self.tokenizer.mask_token, 'یکانان')
-            # tmp = sentence.split()
-            res = self.get_word_in_sentence_probability(
-                sentence, target_word, bert_tokenizer=self.tokenizer, bert_model=self.bertMaskedLM, word_index=mask_index)
+        # print('-------------------------------start---------------------------------------')
+        # print(f'{sentenceMask} \n{targets}')
+        for target_word in words_probabilities:
             results_log.append(
-                (target_word, self.sum_log_probabilities_word(res)))
+                (target_word[0], self.sum_log_probabilities_word(target_word[1])))
+        # print('-------------------------------end-----------------------------------------')
 
         return sorted(results_log, key=lambda x: x[1])
